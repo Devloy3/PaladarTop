691ac9a9b4a1383308deb22ee5e75d6537454559
Eloy Dominguez <eloydominguez3@gmail.com>
2025-12-06 13:11:00 +0100

    Nueva vision del proyecto

--- utils/utils.py
+++ utils/utils.py
@@ -1,15 +1,15 @@
-notas = [
-    ("Restaurante Chino",3,2,5,5,8),
-    ("Manny Fanny",5,5,7,7,9),
-    ("La Madurada",10,10,9,9,6),
-    ("El Tinell", 8, 5.5, 9, 7, 7),
-    ("Campo de Tiro", 2, 3, 6, 2, 8),
-    ("Love Milanesa", 7, 6, 7.5, 5, 7),
-    ("Pizzeria Sagrada Familia", 9.5, 6.5, 7.5, 6, 7),
-    ("Jardinet de Gracia", 7.5, 8, 7, 8, 7.5),
-    ("Lamalva", 10, 7, 8.5, 7, 7),
-    ("Antigua", 7.6, 6.6, 7.6, 9, 5.3),
-    ("Asador Erri Berri", 7, 5.6, 9.3, 9.6, 5.6)
-]
+# notas = [
+#     ("Restaurante Chino",3,2,5,5,8),
+#     ("Manny Fanny",5,5,7,7,9),
+#     ("La Madurada",10,10,9,9,6),
+#     ("El Tinell", 8, 5.5, 9, 7, 7),
+#     ("Campo de Tiro", 2, 3, 6, 2, 8),
+#     ("Love Milanesa", 7, 6, 7.5, 5, 7),
+#     ("Pizzeria Sagrada Familia", 9.5, 6.5, 7.5, 6, 7),
+#     ("Jardinet de Gracia", 7.5, 8, 7, 8, 7.5),
+#     ("Lamalva", 10, 7, 8.5, 7, 7),
+#     ("Antigua", 7.6, 6.6, 7.6, 9, 5.3),
+#     ("Asador Erri Berri", 7, 5.6, 9.3, 9.6, 5.6)
+# ]
 
 
